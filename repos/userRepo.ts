fde05c5680a0441e1c6ae9c5252ab0ac62a66dc8
vladimir <swifrorlilko@gmail.com>
2022-10-21 00:33:12 +0300

    Add user repo

--- repos/userRepo.ts
+++ repos/userRepo.ts
@@ -1,33 +1,83 @@
-import { PrismaClient, User } from "@prisma/client";
+import { Course, PrismaClient, User } from "@prisma/client";
+import UserModel from "../models/user.model";
 
 class UserRepo {
-  private readonly prisma: PrismaClient;
+    private readonly prisma: PrismaClient;
 
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
 
-  async getUserById(id: string) {
-    return this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-  }
+    async getUserById(id: string): Promise<User | null> {
+        return await this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
 
-    async getUserByEmail(email: string) {
-    return this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-  }
+    async getUserByEmail(email: string): Promise<User | null> {
+        return await this.prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
 
-    async createUser(user: User) {
-    return this.prisma.user.create({
-      data: user,
-    });
-  }
+    async createUser(user: UserModel): Promise<User> {
+        return await this.prisma.user.create({
+            data: {
+                username: user.getUsername(),
+                email: user.getEmail(),
+                password: user.getPassword(),
+            },
+        });
+    }
+
+    async updateUser(id: string, userUpdated: UserModel): Promise<User> {
+        return await this.prisma.user.update({
+            where: {
+                id: id,
+            },
+            data: {
+                username: userUpdated.getUsername(),
+                email: userUpdated.getEmail(),
+                password: userUpdated.getPassword(),
+            },
+        });
+    }
+
+    async deleteUser(id: string): Promise<User> {
+        return await this.prisma.user.delete({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return await this.prisma.user.findMany();
+    }
+
+    async getUserCourses(id: string): Promise<Course[]> {
+        const owned = await this.prisma.ownedCourses.findMany({
+            where: {
+                userId: id,
+            },
+        });
+
+        const ownedIds = owned.map(course => course.courseId);
+
+        const courses = await this.prisma.course.findMany({
+            where: {
+                id: {
+                    in: ownedIds,
+                },
+            },
+        });
+
+        return courses;
+   }
 }
 
 export default UserRepo;
